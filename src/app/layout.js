80830d6ace5051948c060d968a9b26853934fe14
Le-Vu-Hoang <bx1536hoang@gmail.com>
2025-05-18 16:14:23 +0700

    fix tailwind

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,15 @@
 //layout cua toan web
 //bo header footer vao day
 'use client'
-import { ToastProvider } from '@/context/toastContext';
+import { ToastProvider } from '@/context/toastContext'
+import 'src/styles/tailwind.css'
 
 export default function RootLayout({ children }) {
   return (
     <html lang="vi">
     <body>
     <ToastProvider>
-      <header>Header người dùng</header>
+      <header className={`text-red-400`}>Header người dùng</header>
       <main>{children}</main>
       <footer>Footer người dùng</footer>
     </ToastProvider>

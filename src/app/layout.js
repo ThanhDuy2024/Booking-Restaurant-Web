bf678f5b994bbb50888004ae218e6c31c4c199f8
Hieu <nguyenhieu96hth@gmail.com>
2025-05-20 18:07:13 +0700

    Header

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,20 @@
 //layout cua toan web
 //bo header footer vao day
-'use client'
+'use client';
 import { ToastProvider } from '@/context/toastContext';
-
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+import '../styles/tailwind.css';
 export default function RootLayout({ children }) {
   return (
     <html lang="vi">
-    <body>
-    <ToastProvider>
-      <header>Header người dùng</header>
-      <main>{children}</main>
-      <footer>Footer người dùng</footer>
-    </ToastProvider>
-    </body>
+      <body>
+        <ToastProvider>
+          <Header />
+          <main>{children}</main>
+          <Footer />
+        </ToastProvider>
+      </body>
     </html>
   );
 }
